5fc3c1a0f6c888117cd842b3f443dba3a7f8102f
mansourab <abdoulaye.mansour@gmail.com>
2021-02-22 14:34:17 +0100

    Ajout de templating pour mieux tester

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,25 +2,22 @@
 
 namespace App\Controller;
 
-use App\Data\SearchData;
-use App\Form\SearchForm;
+
 use App\Repository\MemoireRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="home_home")
+     * @Route("/", name="app_home")
      */
     public function index(MemoireRepository $repo): Response
     {
-        
-
         $latestMemoires = $repo->findLatest();
 
-        return $this->render('layouts/home.html.twig', [
+        return $this->render('layouts/main.html.twig', [
             'latests' => $latestMemoires,
         ]);
     }
